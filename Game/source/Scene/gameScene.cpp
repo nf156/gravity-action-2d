4138280721e877bb3db45c57a310d71abe9a8868
nf156 <nnnfaoto@gmail.com>
2026-09-19 16:31:45 +0900

    fix: 描画フレーム管理をmainに統一

--- Game/source/Scene/gameScene.cpp
+++ Game/source/Scene/gameScene.cpp
@@ -32,7 +32,6 @@ void GameScene::Update(float dt)
 
 void GameScene::Draw(SpriteRenderer& renderer)
 {
-    renderer.Begin(); // フレームで1回
     // 初回だけロード（簡易運用）
     static bool loaded = false;
     if (!loaded && m_tm)
@@ -44,7 +43,6 @@ void GameScene::Draw(SpriteRenderer& renderer)
     }
 
     m_objectManager.DrawAll(renderer);
-    renderer.End(); // フレームで1回
 }
 
 void GameScene::Finalize()
--- Game/source/Scene/titleScene.cpp
+++ Game/source/Scene/titleScene.cpp
@@ -35,7 +35,6 @@ void TitleScene::Draw(SpriteRenderer& renderer)
     }
 
     m_objectManager.DrawAll(renderer);
-    renderer.Flush(); // ƒtƒŒ[ƒ€‚Å1‰ñ
 }
 
 void TitleScene::Finalize()
